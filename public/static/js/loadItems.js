1028673d706fb6faaff519486ff8fde98120cec4
McNobby <teodorbb@gmail.com>
2023-01-24 16:42:47 +0100

    Added support for images

--- public/static/js/loadItems.js
+++ public/static/js/loadItems.js
@@ -4,15 +4,24 @@ const loadItems = async ()=> {
     const items = await (await fetch(`/api/dash/${id}/items`)).json();
     window.items = items;
     items.forEach(item => {
-        const itemContainer = document.createElement('div');
+        const itemTextWrap = document.createElement('div');
+        const itemContainer = document.createElement('div');''
         const itemTitle = document.createElement('h3');
         const itemDescription = document.createElement('p');
+        const imageEl = document.createElement('img')
+
+        imageEl.src = item.image || ''
+        
 
         itemTitle.innerHTML = item.title;
         itemDescription.innerHTML = item.description;
 
-        itemContainer.appendChild(itemTitle);
-        itemContainer.appendChild(itemDescription);
+        item.image && itemContainer.appendChild(imageEl)
+        
+        itemTextWrap.appendChild(itemTitle);
+        itemTextWrap.appendChild(itemDescription);
+        itemContainer.appendChild(itemTextWrap)
+
         itemContainer.id = item._id;
         itemContainer.className = 'item glass';
         itemContainer.onclick = deleteOrOpenLink;
