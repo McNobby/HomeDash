c0b76d02e029098a7481bd10181de3d23761f516
McNobby <teodorbb@gmail.com>
2023-01-24 08:57:36 +0100

    Changed to loopback ip

--- server/dbconnect.js
+++ server/dbconnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    mongoose.connect('mongodb://192.168.166.174:27017/dashboard').catch(err => console.log(err))
+    mongoose.connect('mongodb://127.0.0.1:27017/dashboard').catch(err => console.log(err))
     .then(() => console.log('Connected to DB'))
 }
 
