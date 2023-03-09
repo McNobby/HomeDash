4f119160bff1559e2c59f2b00ba3df40911d0fbe
McNobby <76711087+McNobby@users.noreply.github.com>
2023-03-09 21:03:59 +0100

    feat(global) Created loadning component

---
+++ apps/frontend/src/components/Loading.vue
@@ -0,0 +1,20 @@
+<template>
+    <img class="blue" src="/dash.png" />
+</template>
+
+<style scoped lang="scss">
+@keyframes glow {
+    0% {
+        scale: 1;
+        filter: drop-shadow(0 0 2em #4bc5db);
+    }
+    50% {
+        filter: drop-shadow(0 0 4em #4bc5db);
+        scale: 0.9;
+    }
+    100% {
+        scale: 1;
+        filter: drop-shadow(0 0 2em #4bc5db);
+    }
+}
+</style>
