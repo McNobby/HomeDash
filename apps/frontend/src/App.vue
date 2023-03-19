cf5f12ebbe939d0b6e9d897282e70379d1d27ef4
McNobby <76711087+McNobby@users.noreply.github.com>
2023-03-19 15:29:00 +0100

    Changed backgroudn and base styling

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,5 +1,3 @@
-
-
 <template>
 
      <nav v-if="AuthStore.isLoggedIn()">
--- apps/frontend/src/pages/Home.vue
+++ apps/frontend/src/pages/Home.vue
@@ -1,8 +1,32 @@
 <template>
-    <br>
-    <br>
-    <br>
-    <h1>Hello wolrd</h1>
+    <div class="center">
+        <div class="home">
+            <div class="glass card">
+                <h1>Home</h1>
+                <p>
+                    This is the <strong>Best</strong> Dashboard you have
+                </p>
+            </div>
+            <div class="glass card">
+                <h1>Home</h1>
+                <p>
+                    This is the <strong>Best</strong> Dashboard you have
+                </p>
+            </div>
+            <div class="glass card">
+                <h1>Home</h1>
+                <p>
+                    This is the <strong>Best</strong> Dashboard you have
+                </p>
+            </div>
+            <div class="glass card">
+                <h1>Home</h1>
+                <p>
+                    This is the <strong>Best</strong> Dashboard you have
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
 
 
@@ -14,3 +38,14 @@ export default defineComponent({
     
 </script>
 
+<style scoped lang="scss">
+
+.home{
+    display: grid;
+    width: 90%;
+    gap: 2rem 1rem;
+    margin: 0 auto;
+    grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+}
+
+</style>
